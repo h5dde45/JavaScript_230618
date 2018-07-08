5efab5906120b7b064fecc6e56c5ab12b7f86616
tmvf <tmvf@yandex.ru>
2018-07-08 12:18:44 +0300

    15

---
+++ src/main/resources/templates/ut_06/pl_01/t_01/1.js
@@ -0,0 +1,34 @@
+// let number1 = 10,
+//     string1="s",
+//     bool=true,
+//     nul=null,
+//     indef=undefined;
+//
+// console.log(typeof number1);
+// console.log(number1);
+// console.log(typeof string1);
+// console.log(string1);
+// console.log(typeof bool);
+// console.log(bool);
+// console.log(typeof nul);
+// console.log(nul);
+// console.log(typeof indef);
+// console.log(indef);
+//
+// let obj={"name":"qwe"},
+//     arr=[1,2,3],
+//     regexp= /w+/g,
+//     func=function () {
+//
+//     };
+//
+// console.log(typeof obj);
+// console.log(obj);
+// console.log(typeof arr);
+// console.log(arr);
+// console.log(typeof regexp);
+// console.log(regexp);
+// console.log(typeof func);
+// console.log(func);
+
+
